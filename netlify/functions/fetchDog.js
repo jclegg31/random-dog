20f10d562d04342277482cd74688ad6ce8f9d2fd
julie_c <forechecker@gmail.com>
2024-11-21 15:57:37 -0700

    updated to es modules

--- netlify/functions/fetchDog.js
+++ netlify/functions/fetchDog.js
@@ -1,7 +1,9 @@
-require('dotenv').config()
-const fetch = require('node-fetch')
+import fetch from 'node-fetch'
+import dotenv from 'dotenv'
 
-exports.handler = async function () {
+dotenv.config()
+
+export const handler = async function () {
   const apiKey = process.env.API_KEY
   const url = 'https://api.thedogapi.com/v1/images/search'
 
