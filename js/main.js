1abad42b53798f078e51ee43b51f2d0a14fcd678
julie_c <forechecker@gmail.com>
2024-11-21 16:51:26 -0700

    downgraded node-fetch

--- js/main.js
+++ js/main.js
@@ -9,17 +9,20 @@ function fetchRandomDog() {
   const url = `/.netlify/functions/fetchDog`
 
   fetch(url)
-    .then((res) => res.json()) //parse response as JSON
+    .then((res) => res.json()) // Parse response as JSON
     .then((data) => {
-      console.log(data)
-      const dog = data[0]
-      dogImage.src = dog.url
-      loadingMessage.style.display = 'none'
-      dogImage.style.display = 'block'
-      //document.querySelector('h3').innerText = data.explanation
+      console.log('API Response:', data)
+      if (data && data.length > 0 && data[0].url) {
+        const dog = data[0]
+        dogImage.src = dog.url
+        loadingMessage.style.display = 'none'
+        dogImage.style.display = 'block'
+      } else {
+        loadingMessage.textContent = 'No dog image found. Please try again.'
+      }
     })
     .catch((err) => {
-      console.log(`error ${err}`)
+      console.log(`Error: ${err}`)
       loadingMessage.textContent = 'Failed to load image. Please try again.'
     })
 }
--- netlify/functions/fetchDog.js
+++ netlify/functions/fetchDog.js
@@ -1,24 +1,24 @@
-import dotenv from 'dotenv'
+require('dotenv').config()
+const fetch = require('node-fetch')
 
-dotenv.config()
-
-export const handler = async function () {
-  const fetch = await import('node-fetch')
+exports.handler = async function () {
   const apiKey = process.env.API_KEY
   const url = 'https://api.thedogapi.com/v1/images/search'
 
   try {
-    const response = await fetch.default(url, {
+    const response = await fetch(url, {
       headers: {
         'x-api-key': apiKey,
       },
     })
     const data = await response.json()
+    console.log('Fetched Data:', data) // Log the fetched data
     return {
       statusCode: 200,
       body: JSON.stringify(data),
     }
   } catch (error) {
+    console.error('Error fetching data:', error) // Log the error
     return {
       statusCode: 500,
       body: 'Error fetching data',
